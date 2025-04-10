4f236867a52b8c56e5d7ff77f62569939b5c5c62
Romain Gremaud <romaingremaud@mac.attlocal.net>
2025-04-10 08:18:42 -0400

    Add regex to scan for words

--- script.rb
+++ script.rb
@@ -9,24 +9,25 @@ dictionary = ["below","down","go","going","horn","how","howdy",
 #  "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
 
 def substrings(string,dictionary)
-  # create a a hash
-  string_array = string.split(" ")
-  p string_array
-  
-  hash_array = string_array.reduce(Hash.new(0)) do |word, count|
-    word[count] = 0
+
+  #create an array with string split by spaces
+  #run a filter with the dictionary array with include?, checking if each dictionary word is included
+  #in the string.  Keep string as a string for now
+  p string
+  dictionary_array = dictionary.filter {|word| string.include? word}
+
+  scan = string.scan(/(?=#{"how"})/).count
+  p dictionary_array
+  p scan
+
+  hash_array = dictionary_array.reduce(Hash.new(0)) do |word, quantity|
+    word[quantity] = string.scan(/(?=#{word})/).count
     word
   end
 
-p hash_array
-  # key for hash is  your word or words
-  # value for hash is the number of times that word shows up in the dictionary
-  # needs to search for both the word and any substrings -> below returns 1 for below and low
-  # take the word and scan dictionary
-  # for a string = "howdy how", string.scan(/how/).length returns 2
-  # array.map {|str| str.count(‘a’) }
+  p hash_array
   
 end
 
-substrings("howdy how",dictionary)
+substrings("howdy hotodg",dictionary)
 
